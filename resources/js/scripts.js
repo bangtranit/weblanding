623b4709c7f630abb2f6275f1b996399e22d04d9
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-04-23 00:20:35 +0900

    finished handle sticky menu bar for mobile verson

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -11,5 +11,18 @@ $(document).ready(
                 offset: "600px"
             }
         )
+
+        $(".mobile-nav-icon").click(
+            function(){
+                $(".main-nav").slideToggle(200);
+                if($(".mobile-nav-icon").hasClass("fa-bars")){
+                    $(".mobile-nav-icon").addClass("fa-times");
+                    $(".mobile-nav-icon").removeClass("fa-bars");
+                }else{
+                    $(".mobile-nav-icon").addClass("fa-bars");
+                    $(".mobile-nav-icon").removeClass("fa-times");
+                }
+            }
+        )
     }
 )
