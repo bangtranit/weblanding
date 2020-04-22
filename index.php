090c92b305c1531b72557a9a438e87af3cc7cacb
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-04-23 00:41:26 +0900

    init project php to prepare deploy heroku

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php
-
-public function checklogin{
-    if ($prefix != 'WORKFLOW' || $prefix != 'SALARY'){
-        return next($request);
-    }
-    if ($employee.joined_on < date("Y/m/d")){
-        return $response;
-    }
-}
+<?php include_once("home.html"); ?>
